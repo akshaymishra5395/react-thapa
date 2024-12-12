efabff2c43ef886c73cbc81cc080905b172ce4ce
akshaymishra <akshaymishra5395@gmail.com>
2024-12-12 06:26:47 +0530

    feat:conditional values

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -19,10 +19,15 @@ function NetflixSeries(){
   const name="Thedrma";
   const rating=8.2;
   const summary="dakdhakjdahkjd akjhdakjdakjdh ajkdb akjdbakjdakjdbadajdg ahjdbajdadabdnabdjabda"
-  
+  const age=19
+
   const retGenre=()=>{
     const genre="romCom"
     return genre;
+  }
+  const canWatch=()=>{
+    if(age>=18) return "Watch Now";
+    return "Not Available";
   }
   return (<div>
     <img src='test.jpg' alt='test.jpg' width="40%" height="50%"></img>
@@ -34,6 +39,7 @@ function NetflixSeries(){
     <p>
       Genre:{retGenre()}
     </p>
+    <button>{canWatch()}</button>
   </div>);
 }
 
