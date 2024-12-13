bdd7fcb32965cac31abeb32f5bd10f9400ce1b5b
akshaymishra <akshaymishra5395@gmail.com>
2024-12-13 22:52:28 +0530

    feat:destructure in Jsx

--- react-thapa/src/components/SeriesCard.jsx
+++ react-thapa/src/components/SeriesCard.jsx
@@ -1,15 +1,16 @@
 export const SeriesCard = (props) =>{
+    const {id,img_url,name,rating,description,genre,cast,watch_url} = props.curEle
     return (
-        <li key={props.curEle.id}>
+        <li key={id}>
             <div>
-                <img src={props.curEle.img_url} alt={props.curEle.name} width="40%" height="50%" />
+                <img src={img_url} alt={name} width="40%" height="50%" />
             </div>
-            <h2>Name:{props.curEle.name} </h2>
-            <h3>Rating: {props.curEle.rating} </h3>
-            <p>Summary:{props.curEle.description}</p>
-            <p>Genre:{props.curEle.genre}</p>
-            <p>Cast:{props.curEle.cast}</p>
-            <a href={props.curEle.watch_url} target="_blank">
+            <h2>Name:{name} </h2>
+            <h3>Rating: {rating} </h3>
+            <p>Summary:{description}</p>
+            <p>Genre:{genre}</p>
+            <p>Cast:{cast}</p>
+            <a href={watch_url} target="_blank">
                 <button>Watch Now</button>
             </a>
         </li>
