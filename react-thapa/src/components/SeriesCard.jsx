880baf579b253e786d586e6cb78a93ceed564734
akshaymishra <akshaymishra5395@gmail.com>
2024-12-14 20:04:21 +0530

    feat:use styled-comonents

--- react-thapa/src/components/SeriesCard.jsx
+++ react-thapa/src/components/SeriesCard.jsx
@@ -1,18 +1,37 @@
 import styles from "./Netflix.module.css";
+import styled from "styled-components";
 
 export const SeriesCard = ({curEle}) =>{
     const {id,img_url,name,rating,description,genre,cast,watch_url} = curEle
 
-    const btn_style = {
-        padding: "1.2rem 2.4rem",
-        border: "none",
-        fontSize: "1.6rem",
-        backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
-        color: "var(--btn-color)",
-        fontWeight: "bold",
-        cursor: "pointer",
-    }
-    
+    // const btn_style = { }
+    // const ButtonThapa = styled.button({
+    //   padding: "1.2rem 2.4rem",
+    //   border: "none",
+    //   fontSize: "1.6rem",
+    //   backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+    //   color: "var(--btn-color)",
+    //   fontWeight: "bold",
+    //   cursor: "pointer",
+    // });
+
+    // const btn_style = { }
+    const ButtonThapa = styled.button`
+        padding: 1.2rem 2.4rem;
+        border: none;
+        font-size: 1.6rem;
+        background-color: ${(props) =>
+            props.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+        color: var(--btn-color);
+        font-weight: bold;
+        cursor: pointer;
+    `;
+    //  In styled-components, you can pass a function within the template literal to dynamically set CSS properties based on props or state
+    const Rating = styled.h3`
+        font-size: 1.6rem;
+        color: #7dcea0;
+        text-transform: capitalize;
+    `;
     const ratingClass = rating >= 8.5 ? styles.super_hit: styles.average;
 
     return (
@@ -22,14 +41,16 @@ export const SeriesCard = ({curEle}) =>{
             </div>
             <div className={styles["card-content"]}>
                 <h2>Name:{name} </h2>
-                <h3>Rating: 
+                <Rating>
+                    Rating:
                     <span className={`${styles.rating} ${ratingClass}`}>{rating}</span>
-                </h3>
+                </Rating>
                 <p>Summary:{description}</p>
                 <p>Genre:{genre.join(",")}</p>
                 <p>Cast:{cast.join(",")}</p>
                 <a href={watch_url} target="_blank">
-                    <button style={btn_style}>Watch Now</button>
+                    {/* <button style={btn_style}>Watch Now</button> */}
+                    <ButtonThapa rating={rating}>Watch Now</ButtonThapa>
                 </a>
             </div>
         </li>
